773340aa9d74dca964db9c4191fe773d2ba64aef
GaneshBalaji-04 <balajibarani2004@protonmail.com>
2024-12-31 14:51:15 +0530

    Created index.html, register.html and changes in BCM python file.

--- BrokenAccessControl.py
+++ BrokenAccessControl.py
@@ -1,1 +1,22 @@
 from flask import *
+import bcrypt
+import database
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+@app.route("/submit")
+def submit():
+    name=request.form['name']
+    username=request.form['username']
+    salt=bcrypt.gensalt()
+    password=bcrypt.hasspw(request.form['password'],salt)
+    email=request.form['email']
+    database.new_user(name,username,password,email)
+    return "<h2>The registration is complete<h2>"
+    
--- database.py
+++ database.py
@@ -1,9 +1,9 @@
 import sqlite3
 
-def new_user(username,password):
+def new_user(name,username,password,email):
     conn = sqlite3.connect('Broken-Access-Control/databases/databases/users.db')
     cursor = conn.cursor()
-    cursor.execute('CREATE TABLE IF NOT EXISTS users(username TEXT, password text)')
+    cursor.execute('CREATE TABLE IF NOT EXISTS users(name TEXT,username TEXT, password TEXT, email TEXT)')
     cursor.execute(f'INSERT INTO users VALUES({username},{password})')
     cursor.commit()
 
