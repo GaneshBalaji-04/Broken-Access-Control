16e4dac8b0ccf7281d2da58ea28a1be01fe543d9
GaneshBalaji-04 <balajibarani2004@protonmail.com>
2024-12-31 15:16:34 +0530

    Corrected the exceptions such as commit with cursor

--- database.py
+++ database.py
@@ -1,16 +1,16 @@
 import sqlite3
 
 def new_user(name,username,password,email):
-    conn = sqlite3.connect('Broken-Access-Control/databases/databases/users.db')
+    conn = sqlite3.connect('users.db')
     cursor = conn.cursor()
     cursor.execute('CREATE TABLE IF NOT EXISTS users(name TEXT,username TEXT, password TEXT, email TEXT)')
-    cursor.execute(f'INSERT INTO users VALUES({username},{password})')
-    cursor.commit()
+    cursor.execute('INSERT INTO users (name, username, password, email) VALUES (?, ?, ?, ?)', (name, username, password, email))
+    conn.commit()
 
 def check_user(username,password):
-    conn = sqlite3.connect('Broken-Access-Control/databases/databases/users.db')
+    conn = sqlite3.connect('users.db')
     cursor = conn.cursor()
-    cursor.execute(f"SELECT username,password FROM users WHERE username={username} and password={password}")
+    cursor.execute("SELECT username, password FROM users WHERE username=? AND password=?", (username, password))
     result=cursor.fetchall()
     if result==[]:
         return False
