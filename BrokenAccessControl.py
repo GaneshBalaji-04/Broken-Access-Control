7ab78ccdd414b0a25a052e068c44c83150684882
GaneshBalaji-04 <balajibarani2004@protonmail.com>
2024-12-31 23:26:54 +0530

    Debugging the check of logging in

--- BrokenAccessControl.py
+++ BrokenAccessControl.py
@@ -2,12 +2,13 @@ from flask import *
 import bcrypt
 import database
 
+
 app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route("/register",methods=['POST'])
+@app.route("/register",methods=['POST','GET'])
 def register():
     return render_template('register.html')
 
@@ -20,13 +21,28 @@ def submit():
     password=bcrypt.hashpw(request.form['password'].encode(),salt)
     email=request.form['email']
     database.new_user(name,username,password,email)
-    return "<h2>The registration is complete<h2>"
+    return render_template('regsuccess.html')
 
-@app.route("/login",methods=['POST'])
+@app.route("/login",methods=['POST','GET'])
 def login():
+    return render_template('login.html')
+
+@app.route('/performlogin',methods=['POST','GET'])
+def login_performed():
     username=request.form['username']
     password=bcrypt.hashpw(request.form['password'].encode(),salt)
-    database.check_user(username,password)
+    if database.check_user(username,password):
+        return """
+        <meta http-equiv="refresh" content="5; url=/user">
+        <h2>You have been successfully logged in... You are redirected...<h2>
+        """
+    else:
+        return """
+        <meta http-equiv="refresh" content="3; url=/login">
+        <h2>Your username or password is incorrect..<h2>
+        <h2>You are redirected back to login page...</h2>
+        """
+    
     
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
