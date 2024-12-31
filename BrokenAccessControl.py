4d922fb869490b8632be1ea7002dafea1d755b25
GaneshBalaji-04 <balajibarani2004@protonmail.com>
2024-12-31 15:15:13 +0530

    Performed spell correction in functions

--- BrokenAccessControl.py
+++ BrokenAccessControl.py
@@ -2,21 +2,24 @@ from flask import *
 import bcrypt
 import database
 
+app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route("/register")
+@app.route("/register",methods=['POST','GET'])
 def register():
     return render_template('register.html')
 
-@app.route("/submit")
+@app.route("/submit",methods=['POST','GET'])
 def submit():
     name=request.form['name']
     username=request.form['username']
     salt=bcrypt.gensalt()
-    password=bcrypt.hasspw(request.form['password'],salt)
+    password=bcrypt.hashpw(request.form['password'].encode(),salt)
     email=request.form['email']
     database.new_user(name,username,password,email)
     return "<h2>The registration is complete<h2>"
     
+if __name__ == "__main__":
+    app.run()
