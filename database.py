3af8c3a11d95559a7a190d9c76d07bbaa1cfcb0b
GaneshBalaji-04 <balajibarani2004@protonmail.com>
2025-01-04 15:51:12 +0530

    Modified this to make it prone to sql injection

--- database.py
+++ database.py
@@ -4,13 +4,13 @@ def new_user(name,username,password,email):
     conn = sqlite3.connect('users.db')
     cursor = conn.cursor()
     cursor.execute('CREATE TABLE IF NOT EXISTS users(name TEXT,username TEXT, password TEXT, email TEXT)')
-    cursor.execute('INSERT INTO users (name, username, password, email) VALUES (?, ?, ?, ?)', (name, username, password, email))
+    cursor.execute("INSERT INTO users (name, username, password, email) VALUES ('"+ name + "','" + username + "','" + password + "','" + email + "')")
     conn.commit()
 
 def check_user(username,password):
     conn = sqlite3.connect('users.db')
     cursor = conn.cursor()
-    cursor.execute("SELECT username, password FROM users WHERE username=?", (username))
+    cursor.execute("SELECT username, password FROM users WHERE username='" + username + "' && password = '" + password + "'")
     result=cursor.fetchall()
     if result==[]:
         return False
